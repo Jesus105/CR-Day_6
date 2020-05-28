1cc2dc537fd0523ca2ec792cdd4eb867368b1171
Jesus105 <53441223+Jesus105@users.noreply.github.com>
2020-05-27 22:42:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,23 +18,17 @@ const getMoveNames = (id) =>{
     request.get(`${POKE_URI}/pokemon/${id}`, (error, respons, body)=>{
        
         const pokemon = JSON.parse(body)
-        let Array_moves = []     
-    
+        let Array_moves = []  
+        
+        console.log(`Este es un movimiento de ${pokemon.name}:`)
+        
         for(let i = 0; i < pokemon.moves.length; i++)
         {
     
-            Array_moves.push(pokemon.moves[i].move.name)          
-    
-        }
-    
-        console.log(`Este es un movimiento de ${pokemon.name}:`)
-
-        for(let k = 0; k < Array_moves.length; k++){
+            Array_moves.push(pokemon.moves[i].move.name) 
             console.log(Array_moves[k])
-        }
     
-        
-    
+        }    
          
     }) 
 }
